994f49d39596283d36e9ea283e8f495fac4e557a
Rene <renecabrera@gmail.com>
2022-03-28 18:31:54 -0300

    con manejo exceptions

--- src/app.py
+++ src/app.py
@@ -129,100 +129,138 @@ inspecciones_schema = InspeccionSchema(many=True)
 
 @app.route('/ingreso_vehiculo', methods=['POST'])
 def post_ingreso():
-    ##fields = ('id', 'marca', 'modelo', 'patente', 'anio', 'personaid')
-    """recibido atributos del json en el request"""
-    marca = request.json['marca']
-    modelo = request.json['modelo']
-    patente = request.json['patente']
-    anio = request.json['anio']
-    personaid = request.json['personaid']
-    
-    """envio las variables a la classe Vehiculo """
-    nuevo_vehiculo = Vehiculo(marca, modelo, patente, anio, personaid)
-    """agrego la data a la BD"""
-    db.session.add(nuevo_vehiculo)
-    """hago commit para terminar con la operacion"""
-    db.session.commit()
+    try:
+        ##fields = ('id', 'marca', 'modelo', 'patente', 'anio', 'personaid')
+        """recibido atributos del json en el request"""
+        marca = request.json['marca']
+        modelo = request.json['modelo']
+        patente = request.json['patente']
+        anio = request.json['anio']
+        personaid = request.json['personaid']
+        
+        """envio las variables a la classe Vehiculo """
+        nuevo_vehiculo = Vehiculo(marca, modelo, patente, anio, personaid)
+        """agrego la data a la BD"""
+        db.session.add(nuevo_vehiculo)
+        """hago commit para terminar con la operacion"""
+        db.session.commit()
+        
+        """siempre las funciones deben devolver algo en este caso retorno
+        new_task, que es el registro nuesvo creado """
+        return vehiculo_schema.jsonify(nuevo_vehiculo)
 
-    """siempre las funciones deben devolver algo en este caso retorno
-    new_task, que es el registro nuesvo creado """
-    return vehiculo_schema.jsonify(nuevo_vehiculo)
+    except Exception as errorRamdom:
+        #imprime el error por consola
+        print(errorRamdom)
+        #manda mensaje que hubo un error en formato json
+        return jsonify({"Mesaage": "Hubo un error inesperado al grabar vehiculo"})
 
 @app.route('/ingreso_persona', methods=['POST'])
 def post_persona():
-    #identificacion, nombre, apellido
-    identificacion = request.json['identificacion']
-    nombre = request.json['nombre']
-    apellido = request.json['apellido']
+    try:
+        #identificacion, nombre, apellido
+        identificacion = request.json['identificacion']
+        nombre = request.json['nombre']
+        apellido = request.json['apellido']
   
-    nueva_persona = Persona(identificacion, nombre, apellido)
-    db.session.add(nueva_persona)
-    db.session.commit()
+        nueva_persona = Persona(identificacion, nombre, apellido)
+        db.session.add(nueva_persona)
+        db.session.commit()
 
-    return persona_schema.jsonify(nueva_persona)
+        return persona_schema.jsonify(nueva_persona)
+
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al grabar persona"})
 
 @app.route('/ingreso_revision', methods=['POST'])
 def post_revision():
-    #vehiculoid, aprobado, observaciones, personaiden, fecharevision
-    vehiculoid = request.json['vehiculoid']
-    aprobado = request.json['aprobado']
-    observaciones = request.json['observaciones']
-    personaiden = request.json['personaiden']
-    fecharevision = request.json['fecharevision']
+    try:
+        #vehiculoid, aprobado, observaciones, personaiden, fecharevision
+        vehiculoid = request.json['vehiculoid']
+        aprobado = request.json['aprobado']
+        observaciones = request.json['observaciones']
+        personaiden = request.json['personaiden']
+        fecharevision = request.json['fecharevision']
   
-    nueva_revision = Revision(vehiculoid, aprobado, observaciones, personaiden, fecharevision)
-    db.session.add(nueva_revision)
-    db.session.commit()
+        nueva_revision = Revision(vehiculoid, aprobado, observaciones, personaiden, fecharevision)
+        db.session.add(nueva_revision)
+        db.session.commit()
 
-    return revision_schema.jsonify(nueva_revision)
+        return revision_schema.jsonify(nueva_revision)
+
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al grabar la revision"})
 
 @app.route('/tipo_revision', methods=['POST'])
 def post_tipo_revision():
-    #nombretipo
-    nombretipo = request.json['nombretipo']
-      
-    tipo_revision = Tiporevision(nombretipo)
-    db.session.add(tipo_revision)
-    db.session.commit()
-
-    return tiporevision_schema.jsonify(tipo_revision)
+    try:
+        #nombretipo
+        nombretipo = request.json['nombretipo']
+        tipo_revision = Tiporevision(nombretipo)
+        db.session.add(tipo_revision)
+        db.session.commit()
+        return tiporevision_schema.jsonify(tipo_revision)
+    
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al grabar tipo de revision"})
 
 @app.route('/ingreso_inspeccion', methods=['POST'])
 def post_inspeccion():
-    #fields = ('id', 'revisionid', 'tipoinspeccionid', 'observaciones', 'estado', 'personaiden')
-    revisionid = request.json['revisionid']
-    tipoinspeccionid = request.json['tipoinspeccionid']
-    observaciones = request.json['observaciones']
-    estado = request.json['estado']
-    personaiden = request.json['personaiden']
+    try:
+        #fields = ('id', 'revisionid', 'tipoinspeccionid', 'observaciones', 'estado', 'personaiden')
+        revisionid = request.json['revisionid']
+        tipoinspeccionid = request.json['tipoinspeccionid']
+        observaciones = request.json['observaciones']
+        estado = request.json['estado']
+        personaiden = request.json['personaiden']
   
-    nueva_inspeccion = Inspeccion(revisionid, tipoinspeccionid, observaciones, estado, personaiden)
-    db.session.add(nueva_inspeccion)
-    db.session.commit()
+        nueva_inspeccion = Inspeccion(revisionid, tipoinspeccionid, observaciones, estado, personaiden)
+        db.session.add(nueva_inspeccion)
+        db.session.commit()
 
-    return inspeccion_schema.jsonify(nueva_inspeccion)
+        return inspeccion_schema.jsonify(nueva_inspeccion)
+
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al grabar la inspeccion"})
 
 @app.route('/borrar_inspeccion/<id>', methods=['DELETE'])
 def delete_inspeccion(id):
-    inspeccion = Inspeccion.query.get(id)
-    db.session.delete(inspeccion)
-    db.session.commit()
-    
-    return inspeccion_schema.jsonify(inspeccion)
+    try:
+        inspeccion = Inspeccion.query.get(id)
+        #valida si es true inspeccion, sino 
+        if inspeccion:
+            db.session.delete(inspeccion)
+            db.session.commit()
+            return inspeccion_schema.jsonify(inspeccion)
 
+        return jsonify({"messages": "No existe la inspeccion con ese ID"})
+
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al borrar la inspeccion"})
+ 
 @app.route('/consulta_revision/<id>', methods=['GET'])
 def get_revision(id):
-    """se hace la consulta con el id"""
-     
-    revision_por_patente = (db.session.query(Revision)
-                            .join(Vehiculo)
-                            .filter(Vehiculo.id == id)
-                            ).all()
-    
-    db.session.commit()
-    
-    return revisiones_schema.jsonify(revision_por_patente)
+    try:
+        """se hace la consulta con el id"""
+        revision_por_patente = (db.session.query(Revision)
+                                .join(Vehiculo)
+                                .filter(Vehiculo.id == id)
+                                ).all()
+        #valida si la consulta tuvo resultados                        
+        if revision_por_patente:
+            db.session.commit()
+            return revisiones_schema.jsonify(revision_por_patente)
 
+        return jsonify({"messages": "Codigo de vehiculo no existe"})
+
+    except Exception as errorRamdom:
+        print(errorRamdom)
+        return jsonify({"Mesaage": "Hubo un error inesperado al consultar por las revisiones"})
 
 """============== Server =============================== Server ==============================="""
 if __name__ == '__main__':
